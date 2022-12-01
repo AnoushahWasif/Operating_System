0bf08e7ea508cad106bb12432db9ee8c6b5e1116
AnoushahWasif <75376557+AnoushahWasif@users.noreply.github.com>
2022-12-01 17:27:59 +0500

    Add files via upload

---
+++ Lab7/5.sh
@@ -0,0 +1,20 @@
+#program
+echo Enter any marks
+read marks
+if(($marks>80))
+ then
+ echo Your Grade is A
+ elif(($marks>70))
+  then
+  echo Your Grade is B
+  elif(($marks>70))
+   then
+   echo Your Grade is B
+   elif(($marks>50))
+     then
+     echo Your Grade is C
+     else
+     echo Your Grade is F
+
+fi
+
